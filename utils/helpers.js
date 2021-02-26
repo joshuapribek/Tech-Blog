bc116df844a8ae6763328faf73f24a845115e3b9
Joshua Pribek <joshpribek@icloud.com>
2021-02-25 19:40:59 -0800

    utils folder

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,14 +1,5 @@
 module.exports = {
   format_date: date => {
-    return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-      date
-    ).getFullYear()}`;
-  },
-  format_plural: (word, amount) => {
-      if (amount !== 1) {
-        return `${word}s`;
-      }
-  
-      return word;
+    return `${date.getMonth() + 1}/${date.getDate()}/${date.getFullYear()}`;
   }
-}
+};
